69976f513973d48106423f51d900908165db0dac
Lincoln-Brown <links1155@gmail.com>
2019-11-21 08:37:15 -0500

    exercise 4 completed

--- world.php
+++ world.php
@@ -18,10 +18,29 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
+
+
 ?>
 
-<ul>
+<table>
+ <tr>
+	<th>name</th>
+	<th>Continent</th>
+	<th>Independence Year</th>
+	<th>Head of State </th>
+
+ </tr>
+	
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <tr>
+
+  	<td><?= $row['name']?><td>
+  	<td><?= $row['continent']?><td>
+  	<td><?= $row['independence_year']?><td>
+  	<td><?= $row['head_of_state']?><td>
+
+ </tr>
+  	
+ 
 <?php endforeach; ?>
-</ul>
+</table>
